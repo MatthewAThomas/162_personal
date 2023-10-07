2d5cf5b52e2373c9c074e87430d1ccf6f6c9694e
Hyeong <simony1124@berkeley.edu>
2023-10-07 22:49:36 +0000

    saving changes

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -32,7 +32,9 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
     f->eax = args[1];
     printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
     process_exit();
-  } 
+  } else if (args[0] == ) { // TODO what is practice syscall number?
+    f->eax = ++(args[1]);
+  }
   /*
   // Start of File Syscall
   else if (args[0] == SYS_CREATE) {
